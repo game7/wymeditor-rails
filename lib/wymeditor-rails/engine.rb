3353b492cc8ebaa16027dc73ebefa0285b91a62f
Chris Woodall <cmwoodall@game7.net>
2012-03-30 13:38:05 -0700

    add precompile paths

--- lib/wymeditor-rails/engine.rb
+++ lib/wymeditor-rails/engine.rb
@@ -1,5 +1,12 @@
 module WymeditorRails
   class Engine < ::Rails::Engine
     # this just lets rails know to load from vendor/assets
+    initializer "wymeditor-rails.assets.precompile" do |app|
+      app.config.assets.precompile += [
+        "wymeditor/iframe/**/*",
+        "wymeditor/lang/en.js",
+        "jquery.wymeditor.js"
+      ]
+    end
   end
 end
